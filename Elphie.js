529f2a3a5d359db26d58591b4d1f09a0e516589a
banitlgus <banitlgus@gmail.com>
2025-11-21 13:45:17 +0900

    change background color

--- Elphie.js
+++ Elphie.js
@@ -18,13 +18,13 @@ class Green extends Magic {
     }
 
     static addNoise() {
-        let noiseScale = 0.02;
+        let noiseScale = 0.009;
 
-        for (let y = 0; y < height; y +=4) {
-            for (let x = 0; x < width; x +=4) {
-                let c = noise(x*noiseScale, y*noiseScale);
-                fill(0, 120+c*50, 0, 1.5);
-                rect(x, y, 12);
+        for (let y = 0; y < height; y +=5) {
+            for (let x = 0; x < width; x +=5) {
+                let c = 25*noise(x*noiseScale, y*noiseScale, frameCount*noiseScale);
+                fill(c*12, 7);
+                rect(x, y, 10);
 
             }
         }
--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let magic;
+let backgroundColor;
 function setup() {
   createCanvas(windowWidth, 400);
   noStroke();
@@ -6,7 +7,15 @@ function setup() {
 }
 
 function draw() {
-  background(0);
+  if (mouseIsPressed) {
+    background(255,200,250);
+    if (keyIsPressed) {
+      background(0,255,0)
+    }
+  } else {
+    background(255);
+  }
+  // background(0);
 
   let force = createVector(0, 0.05);
   magic.addForce(force);
